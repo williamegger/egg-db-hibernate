5cc99e58547f5ecd5fd6f800e064a2f208ce007d
williamegger <625892330@qq.com>
2017-06-15 11:47:57 +0800

    修改HibernateUtil

--- src/com/egg/db/hibernate/HibernateUtil.java
+++ src/com/egg/db/hibernate/HibernateUtil.java
@@ -1,8 +1,5 @@
 package com.egg.db.hibernate;
 
-import java.io.File;
-
-import org.hibernate.HibernateException;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
@@ -13,14 +10,13 @@ import org.slf4j.LoggerFactory;
  * Hibernate工具类
  */
 public class HibernateUtil {
-	private static String current_dir = HibernateUtil.class.getClassLoader().getResource("").getPath() + "hibernate.cfg.xml";
+	// private static String current_dir = HibernateUtil.class.getClassLoader().getResource("").getPath() + "hibernate.cfg.xml";
 	private static Logger LOG = LoggerFactory.getLogger(HibernateUtil.class);
 
 	private static SessionFactory sessionFactory;
 	private static final ThreadLocal<Session> currentSession = new ThreadLocal<Session>();
 	private static final Owner trueOwner = new Owner(true);
 	private static final Owner fakeOwner = new Owner(false);
-	private static final boolean isLife = false;
 
 	public static Object createSession() throws Exception {
 		Session session = (Session) currentSession.get();
@@ -32,38 +28,31 @@ public class HibernateUtil {
 		return fakeOwner;
 	}
 
-	public synchronized static void closeSession(Object ownership) throws Exception {
-		if (((Owner) ownership).identity) {
+	public synchronized static void closeSession(Object ownership) {
+		if (ownership != null && ((Owner) ownership).identity) {
 			Session session = (Session) currentSession.get();
-			session.flush();
-			session.close();
+			if (session != null) {
+				try {
+					session.close();
+				} catch (Exception e) {
+				}
+			}
 			currentSession.set(null);
 		}
 	}
 
-	public synchronized static Session getSession() throws HibernateException {
+	public synchronized static Session getSession() {
 		return (Session) currentSession.get();
 	}
 
 	private synchronized static SessionFactory getSessionFactory() {
 		try {
 			if (sessionFactory == null) {
-				if (isLife) {
-					Configuration cc = new Configuration().configure();
-					sessionFactory = cc.configure(new File(current_dir)).buildSessionFactory();
-				} else {
-					Configuration cc = new Configuration();
-					sessionFactory = cc.configure().buildSessionFactory();
-				}
-			}
-			if (sessionFactory == null) {
-				LOG.error("Failed to buildSessionFactory, sessionFactory is null.");
+				sessionFactory = new Configuration().configure().buildSessionFactory();
 			}
 			return sessionFactory;
-		} catch (HibernateException e) {
+		} catch (Exception e) {
 			LOG.error("getSessionFactory", e);
-		} catch (Exception err) {
-			LOG.error("getSessionFactory", err);
 		}
 		return null;
 	}
